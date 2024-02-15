df478f4c91ece11355dc81e0907d232a59712cd1
Chamara Ariyarathne <chamaraanu@users.noreply.github.com>
2024-02-16 09:49:46 +1100

    Minimalized

--- scripts/deploy-minimal-trex.ts
+++ scripts/deploy-minimal-trex.ts
@@ -31,7 +31,6 @@ export async function deployIdentityProxy(implementationAuthority: Contract['add
  * identityRegistry	  old	old	new	old	old
  * tokenOID	          old	new	old	old	old
  * token	            X	  X	  X	  Can	Can
- * bindIdentityRegistry				    X	  Can
  */
 async function main() {
 
@@ -47,35 +46,13 @@ async function main() {
   console.log("ClaimIssuerSigningKey: ", claimIssuerSigningKey.address)
   console.log("AliceActionKey: ", aliceActionKey.address)
 
-  const identityImplementationAuthority = await ethers.getContractAt("ImplementationAuthority","0x3191ec3FEBf6F3D26801544D1fd116b53d12b10A")
   const trexImplementationAuthority = await ethers.getContractAt("TREXImplementationAuthority","0xf7dc73894F037ddC87fa327b0912141b3d881278")
-  const claimTopicsRegistry = await ethers.getContractAt("ClaimTopicsRegistry","0xB2dcF08161e962A71c4940Ff3b5EfBaFb71CeE67")
-  const trustedIssuersRegistry = await ethers.getContractAt("TrustedIssuersRegistry","0xA03019013cD4c622981A8F03bEe81522A714C067")
-  // const identityRegistryStorage = await ethers.getContractAt("IdentityRegistryStorage","0x5e8f7FC28c3090A437B9066e299C8b4b4203F1B3")
-  // const defaultCompliance = await ethers.getContractAt("DefaultCompliance","0x63aF369E38E56B26695CFE334fCA62FF144DD52F")
   const identityRegistry = await ethers.getContractAt("IdentityRegistry","0x41B07810A74cD65D26823A6b0282DA39B0b620A3")
   const tokenOID = await ethers.getContractAt("Identity","0xacf8b615918263fFC35B5524c3e1b2303A1118eE")
 
   // Deploy implementations
-
-  const identityRegistryStorage = await ethers
-    .deployContract('IdentityRegistryStorageProxy', [trexImplementationAuthority.address], deployer)
-    .then(async (proxy) => ethers.getContractAt('IdentityRegistryStorage', proxy.address));
-  console.log("identityRegistryStorage: ", identityRegistryStorage.address)
-
   const defaultCompliance = await ethers.deployContract('DefaultCompliance', deployer);
   console.log("defaultCompliance: ", defaultCompliance.address)
-  // const identityRegistry = await ethers
-  //   .deployContract(
-  //     'IdentityRegistryProxy',
-  //     [trexImplementationAuthority.address, trustedIssuersRegistry.address, claimTopicsRegistry.address, identityRegistryStorage.address],
-  //     deployer,
-  //   )
-  //   .then(async (proxy) => ethers.getContractAt('IdentityRegistry', proxy.address));
-  // console.log("identityRegistry: ", identityRegistry.address)
-
-  // const tokenOID = await deployIdentityProxy(identityImplementationAuthority.address, tokenIssuer.address, deployer);
-  // console.log("tokenOID: ", tokenOID.address)
 
   const tokenName = 'TREXDINO2';
   const tokenSymbol = 'TREX2';
@@ -96,9 +73,6 @@ async function main() {
     )
     .then(async (proxy) => ethers.getContractAt('Token', proxy.address)); 
   console.log("token: ", token.address)
-
-  await identityRegistryStorage.connect(deployer).bindIdentityRegistry(identityRegistry.address);
-  console.log("identityRegistryStorage.bindIdentityRegistry")
 
   await token.connect(deployer).addAgent(tokenAgent.address);
   console.log("token.addAgent")
